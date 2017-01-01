25f3af32b263551a18c3838575311fa0084c3b43
Keith Tang <keithsktang@gmail.com>
2017-01-01 10:19:48 -0500

    add framworkless responsive base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def hello 
-    render html: "hello, world"
+  def index
+  render :file => 'index.html'
   end
 end
